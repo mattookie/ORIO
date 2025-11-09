3bd50526a23bd333764b3a9dd3533d7f2db09b56
mattookie <mattdumrongthai@gmail.com>
2025-11-09 12:48:08 -0600

    boooooo

--- ORIO/src/controller.cpp
+++ ORIO/src/controller.cpp
@@ -8,3 +8,20 @@ const char* password = "tanisfat";
 WebServer server(80); // Creates webserver on port 80
 
 const char* HTML_CONTENT = R"rawliteral(
+<!DOCTYPE html>
+
+<!-- This contains the meta data about the document -->
+<head>
+    <meta charset = "UTF-8">
+
+    <!-- This makes the website viewable on a mobile screen opposed to a desktop -->
+    <
+
+    <title> ORIO Star Tracker</title>
+
+
+
+</head>
+
+
+)rawliteral";
